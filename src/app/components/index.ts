e4788c82acc1e6f4a735000ea3d274e0c88746f8
Admin <108695191+mgsbb@users.noreply.github.com>
2024-01-18 22:06:40 +0530

    Fix deploy errors

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,4 @@
 import Navbar from './Navbar';
 import PostCard from '../(home)/posts/components/PostCard';
-import SearchForm from './SearchForm';
-import SortForm from './SortForm';
 
-export { Navbar, PostCard, SearchForm, SortForm };
+export { Navbar, PostCard };
